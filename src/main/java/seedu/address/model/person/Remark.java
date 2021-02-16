9e6db52e46962dee65380ed260d6338e768219de
Jay Chen <jaychenyj@gmail.com>
2021-02-16 20:48:24 +0800

    Update Person to support remark field, update storage and finalise ui

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Create a remark object.
+     * @param remark User input for remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
